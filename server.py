381c51b9aa32f587918f524106bc1cb5ef216113
JothishKamal <jothishkamal@gmail.com>
2024-01-30 21:12:48 +0530

    a

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 import os
 from PIL import Image
@@ -25,12 +25,9 @@ def compress_and_save_image(image, file_path):
     with open(file_path, 'wb') as f:
         f.write(img_buffer.getvalue())
 
-
-
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
 
 # Directory where uploaded files will be stored
 UPLOAD_FOLDER = 'uploads'
@@ -40,7 +37,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
 
 # Ensure the upload folder exists
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)   
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -58,9 +55,9 @@ def upload_file():
         # Save the file with the barcode as the filename and keep the original extension
         filename = f"{barcode}{os.path.splitext(file.filename)[1].lower()}"
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        
+
         compress_and_save_image(Image.open(file), file_path)
-        
+
         return jsonify(success=True, message='File uploaded successfully', filename=filename), 200
     else:
         return jsonify(success=False, message='Allowed file types are png, jpg, jpeg, gif'), 400
@@ -69,9 +66,8 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-from flask import send_file
-
 @app.route('/download/<filename>')
 def download_file(filename):
-    # Assuming the images are stored in the UPLOAD_FOLDER directory
+    # Serve the file for download from the UPLOAD_FOLDER directory
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
+
